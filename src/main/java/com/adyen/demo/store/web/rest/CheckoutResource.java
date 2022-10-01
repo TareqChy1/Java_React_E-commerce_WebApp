5af950e78850a5a4cb5034ee8685dd812ff3a76b
TareqChy1 <tareqmdrabiulhossainchy@gmail.com>
2022-10-02 01:47:56 +0200

    importing version(0.1.0)

---
+++ src/main/java/com/adyen/demo/store/web/rest/CheckoutResource.java
@@ -0,0 +1,229 @@
+package com.adyen.demo.store.web.rest;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+import com.adyen.Client;
+import com.adyen.demo.store.domain.ShoppingCart;
+import com.adyen.demo.store.domain.enumeration.OrderStatus;
+import com.adyen.demo.store.security.SecurityUtils;
+import com.adyen.demo.store.service.ShoppingCartService;
+import com.adyen.demo.store.web.rest.errors.EntityNotFoundException;
+import com.adyen.enums.Environment;
+import com.adyen.model.Amount;
+import com.adyen.model.checkout.*;
+import com.adyen.model.modification.CancelOrRefundRequest;
+import com.adyen.model.modification.ModificationResult;
+import com.adyen.service.Checkout;
+import com.adyen.service.Modification;
+import com.adyen.service.exception.ApiException;
+
+/**
+ * REST controller for using Adyen checkout API
+ */
+@RestController
+@RequestMapping("/api")
+public class CheckoutResource {
+    private final Logger log = LoggerFactory.getLogger(CheckoutResource.class);
+
+    @Value("${ADYEN_MERCHANT_ACCOUNT}")
+    private String merchantAccount;
+    @Value("${ADYEN_CLIENT_KEY}")
+    private String clientKey;
+
+    private final Checkout checkout;
+    private final Modification modification;
+    private final ShoppingCartService shoppingCartService;
+
+    public CheckoutResource(final ShoppingCartService shoppingCartService,
+                            @Value("${ADYEN_API_KEY}") String apiKey) {
+        this.shoppingCartService = shoppingCartService;
+        Client client = new Client(apiKey, Environment.TEST);
+        this.checkout = new Checkout(client);
+        this.modification = new Modification(client);
+    }
+
+    /**
+     * {@code GET  /checkout/config} : Get secure config
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the config map.
+     */
+    @GetMapping("/checkout/config")
+    public ResponseEntity<Map<String, String>> config() {
+        Map<String, String> conf = new HashMap<>();
+        conf.put("environment", "test");
+        conf.put("clientKey", clientKey);
+        return ResponseEntity.ok()
+            .body(conf);
+    }
+
+    /**
+     * {@code POST  /checkout/payment-methods} : Get valid payment methods.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the paymentMethods response.
+     * @throws EntityNotFoundException when user is not found.
+     * @throws IOException             from Adyen API.
+     * @throws ApiException            from Adyen API.
+     */
+    @PostMapping("/checkout/payment-methods")
+    public ResponseEntity<PaymentMethodsResponse> paymentMethods() throws IOException, ApiException {
+        PaymentMethodsRequest paymentMethodsRequest = new PaymentMethodsRequest();
+        paymentMethodsRequest.setMerchantAccount(merchantAccount);
+        paymentMethodsRequest.setCountryCode("NL");
+        paymentMethodsRequest.setShopperLocale("nl-NL");
+        paymentMethodsRequest.setChannel(PaymentMethodsRequest.ChannelEnum.WEB);
+
+        Amount amount = getAmountFromCart(getUserCart());
+        paymentMethodsRequest.setAmount(amount);
+        log.debug("REST request to get Adyen payment methods {}", paymentMethodsRequest);
+        PaymentMethodsResponse response = checkout.paymentMethods(paymentMethodsRequest);
+        return ResponseEntity.ok()
+            .body(response);
+    }
+
+    /**
+     * {@code POST  /checkout/initiate-payment} : Make a payment.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with req the paymentMethods response.
+     * @throws EntityNotFoundException when user is not found.
+     * @throws IOException             from Adyen API.
+     * @throws ApiException            from Adyen API.
+     */
+    @PostMapping("/checkout/initiate-payment")
+    public ResponseEntity<PaymentsResponse> payments(@RequestHeader("referer") String referer, @RequestBody PaymentsRequest req, HttpServletRequest request) throws IOException, ApiException {
+        PaymentsRequest paymentRequest = new PaymentsRequest();
+        paymentRequest.setMerchantAccount(merchantAccount);
+        paymentRequest.setCountryCode("NL");
+        paymentRequest.setShopperLocale("nl-NL");
+        paymentRequest.setChannel(PaymentsRequest.ChannelEnum.WEB);
+        URL refURL = new URL(referer);
+        String originalHost = refURL.getProtocol() + "://" + refURL.getHost() + (refURL.getPort() >= 80 ? ":" + refURL.getPort() : "");
+        ShoppingCart activeCart = getUserCart();
+        String returnUrl = originalHost + "/api/checkout/redirect?cartId=" + activeCart.getId() + "&referer=" + referer + "&paymentType=" + req.getPaymentMethod().getType();
+        String orderRef = UUID.randomUUID().toString();
+        paymentRequest.setReturnUrl(returnUrl);
+        paymentRequest.setReference(orderRef);
+        paymentRequest.setPaymentMethod(req.getPaymentMethod());
+        // required for 3ds2 native flow
+        paymentRequest.setAdditionalData(Collections.singletonMap("allow3DS2", "true"));
+        paymentRequest.setOrigin(req.getOrigin());
+        // required for 3ds2 flow
+        paymentRequest.setBrowserInfo(req.getBrowserInfo());
+        // required by some issuers for 3ds2
+        paymentRequest.setShopperIP(request.getRemoteAddr());
+
+        paymentRequest.setAmount(getAmountFromCart(activeCart));
+        log.debug("REST request to make Adyen payment {}", paymentRequest);
+        PaymentsResponse response = checkout.payments(paymentRequest);
+
+        return ResponseEntity.ok()
+            .body(response);
+    }
+
+    /**
+     * {@code POST  /checkout/submit-additional-details} : Make a payment.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the paymentMethods response.
+     * @throws EntityNotFoundException when user is not found.
+     * @throws IOException             from Adyen API.
+     * @throws ApiException            from Adyen API.
+     */
+    @PostMapping("/checkout/submit-additional-details")
+    public ResponseEntity<PaymentsDetailsResponse> payments(@RequestBody PaymentsDetailsRequest detailsRequest) throws IOException, ApiException {
+        log.debug("REST request to make Adyen payment details {}", detailsRequest);
+        PaymentsDetailsResponse response = checkout.paymentsDetails(detailsRequest);
+        return ResponseEntity.ok()
+            .body(response);
+    }
+
+    /**
+     * {@code POST  /checkout/refund-payment} : Cancel and Refund a payment.
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the paymentMethods response.
+     * @throws EntityNotFoundException when user is not found.
+     * @throws IOException             from Adyen API.
+     * @throws ApiException            from Adyen API.
+     */
+    @PostMapping("/checkout/refund-payment")
+    public ResponseEntity<ModificationResult> refund(@RequestBody ShoppingCart cart) throws IOException, ApiException {
+        log.debug("REST request to cancel payment {}", cart);
+        CancelOrRefundRequest req = new CancelOrRefundRequest();
+        req.setMerchantAccount(merchantAccount);
+        req.setOriginalReference(cart.getPaymentReference());
+        req.setReference(UUID.randomUUID().toString());
+        ModificationResult modificationResult = modification.cancelOrRefund(req);
+        // update the shopping cart with ref & status
+        cart = shoppingCartService.findOne(cart.getId()).orElseThrow(() -> new EntityNotFoundException("Cart"));
+        cart.setStatus(OrderStatus.REFUND_INITIATED);
+        cart.setPaymentModificationReference(modificationResult.getPspReference());
+        shoppingCartService.save(cart);
+
+        return ResponseEntity.ok()
+            .body(modificationResult);
+    }
+
+    /**
+     * {@code GET  /checkout/redirect} : Handle redirect during payment.
+     *
+     * @return the {@link RedirectView} with status {@code 302}
+     * @throws IOException  from Adyen API.
+     * @throws ApiException from Adyen API.
+     */
+    @GetMapping("/checkout/redirect")
+    public RedirectView redirect(@RequestParam String redirectResult, @RequestParam String cartId, @RequestParam String referer, @RequestParam String paymentType) throws IOException, ApiException {
+        PaymentsDetailsRequest detailsRequest = new PaymentsDetailsRequest();
+        detailsRequest.setDetails(Collections.singletonMap("redirectResult", redirectResult));
+        return getRedirectView(Long.valueOf(cartId), referer, paymentType, detailsRequest);
+    }
+
+    private RedirectView getRedirectView(final Long cartId, final String referer, final String paymentType, final PaymentsDetailsRequest detailsRequest) throws ApiException, IOException {
+        log.debug("REST request to handle payment redirect {}", detailsRequest);
+
+        PaymentsDetailsResponse response = checkout.paymentsDetails(detailsRequest);
+        String redirectURL = referer + "/status/";
+        switch (response.getResultCode()) {
+            case AUTHORISED:
+                shoppingCartService.updateCartWithPayment(cartId, paymentType, response.getPspReference(), OrderStatus.PAID);
+                redirectURL += "success";
+                break;
+            case PENDING:
+            case RECEIVED:
+                shoppingCartService.updateCartWithPayment(cartId, paymentType, response.getPspReference(), OrderStatus.PENDING);
+                redirectURL += "pending";
+                break;
+            case REFUSED:
+                redirectURL += "failed";
+                break;
+            default:
+                redirectURL += "error";
+                break;
+        }
+        return new RedirectView(redirectURL + "?reason=" + response.getResultCode());
+    }
+
+    private ShoppingCart getUserCart() {
+        String user = SecurityUtils.getCurrentUserLogin().orElseThrow(() -> new EntityNotFoundException("User"));
+        return shoppingCartService.findActiveCartByUser(user);
+    }
+
+    private Amount getAmountFromCart(ShoppingCart activeCart) {
+        Amount amount = new Amount();
+        amount.setCurrency("EUR");
+        amount.setValue(toMinorUnit(activeCart.getTotalPrice()));
+        return amount;
+    }
+
+    private Long toMinorUnit(BigDecimal val) {
+        return val.longValue() * 100L;
+    }
+}
---
+++ src/main/java/com/adyen/demo/store/web/rest/ShoppingCartResource.java
@@ -0,0 +1,269 @@
+package com.adyen.demo.store.web.rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import com.adyen.demo.store.domain.ShoppingCart;
+import com.adyen.demo.store.domain.enumeration.OrderStatus;
+import com.adyen.demo.store.repository.ShoppingCartRepository;
+import com.adyen.demo.store.security.AuthoritiesConstants;
+import com.adyen.demo.store.security.SecurityUtils;
+import com.adyen.demo.store.service.ShoppingCartService;
+import com.adyen.demo.store.web.rest.errors.BadRequestAlertException;
+import com.adyen.demo.store.web.rest.errors.EntityNotFoundException;
+
+import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.ResponseUtil;
+
+/**
+ * REST controller for managing {@link com.adyen.demo.store.domain.ShoppingCart}.
+ */
+@RestController
+@RequestMapping("/api")
+public class ShoppingCartResource {
+
+    private final Logger log = LoggerFactory.getLogger(ShoppingCartResource.class);
+
+    private static final String ENTITY_NAME = "shoppingCart";
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+    private final ShoppingCartService shoppingCartService;
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    public ShoppingCartResource(ShoppingCartService shoppingCartService, ShoppingCartRepository shoppingCartRepository) {
+        this.shoppingCartService = shoppingCartService;
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
+    /**
+     * {@code POST  /shopping-carts} : Create a new shoppingCart.
+     *
+     * @param shoppingCart the shoppingCart to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new shoppingCart, or with status {@code 400 (Bad Request)} if the shoppingCart has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("/shopping-carts")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
+    public ResponseEntity<ShoppingCart> createShoppingCart(@Valid @RequestBody ShoppingCart shoppingCart) throws URISyntaxException {
+        log.debug("REST request to save ShoppingCart : {}", shoppingCart);
+        if (shoppingCart.getId() != null) {
+            throw new BadRequestAlertException("A new shoppingCart cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        ShoppingCart result = shoppingCartService.save(shoppingCart);
+        return ResponseEntity
+            .created(new URI("/api/shopping-carts/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PUT  /shopping-carts/:id} : Updates an existing shoppingCart.
+     *
+     * @param id           the id of the shoppingCart to save.
+     * @param shoppingCart the shoppingCart to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated shoppingCart,
+     * or with status {@code 400 (Bad Request)} if the shoppingCart is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the shoppingCart couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PutMapping("/shopping-carts/{id}")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
+    public ResponseEntity<ShoppingCart> updateShoppingCart(
+        @PathVariable(value = "id", required = false) final Long id,
+        @Valid @RequestBody ShoppingCart shoppingCart
+    ) throws URISyntaxException {
+        log.debug("REST request to update ShoppingCart : {}, {}", id, shoppingCart);
+        if (shoppingCart.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, shoppingCart.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!shoppingCartRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        ShoppingCart result = shoppingCartService.save(shoppingCart);
+        return ResponseEntity
+            .ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, shoppingCart.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PATCH  /shopping-carts/:id} : Partial updates given fields of an existing shoppingCart, field will ignore if it is null
+     *
+     * @param id           the id of the shoppingCart to save.
+     * @param shoppingCart the shoppingCart to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated shoppingCart,
+     * or with status {@code 400 (Bad Request)} if the shoppingCart is not valid,
+     * or with status {@code 404 (Not Found)} if the shoppingCart is not found,
+     * or with status {@code 500 (Internal Server Error)} if the shoppingCart couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PatchMapping(value = "/shopping-carts/{id}", consumes = "application/merge-patch+json")
+    public ResponseEntity<ShoppingCart> partialUpdateShoppingCart(
+        @PathVariable(value = "id", required = false) final Long id,
+        @NotNull @RequestBody ShoppingCart shoppingCart
+    ) throws URISyntaxException {
+        log.debug("REST request to partial update ShoppingCart partially : {}, {}", id, shoppingCart);
+        if (shoppingCart.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, shoppingCart.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!shoppingCartRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        Optional<ShoppingCart> result = shoppingCartService.partialUpdate(shoppingCart);
+
+        return ResponseUtil.wrapOrNotFound(
+            result,
+            HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, shoppingCart.getId().toString())
+        );
+    }
+
+    /**
+     * {@code GET  /shopping-carts} : get all the shoppingCarts.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of shoppingCarts in body.
+     */
+    @GetMapping("/shopping-carts")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
+    public List<ShoppingCart> getAllShoppingCarts() {
+        log.debug("REST request to get all ShoppingCarts");
+        return shoppingCartService.findAll();
+    }
+
+    /**
+     * {@code GET  /shopping-carts/:id} : get the "id" shoppingCart.
+     *
+     * @param id the id of the shoppingCart to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the shoppingCart, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/shopping-carts/{id}")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
+    public ResponseEntity<ShoppingCart> getShoppingCart(@PathVariable Long id) {
+        log.debug("REST request to get ShoppingCart : {}", id);
+        Optional<ShoppingCart> shoppingCart = shoppingCartService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(shoppingCart);
+    }
+
+    /**
+     * {@code DELETE  /shopping-carts/:id} : delete the "id" shoppingCart.
+     *
+     * @param id the id of the shoppingCart to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/shopping-carts/{id}")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
+    public ResponseEntity<Void> deleteShoppingCart(@PathVariable Long id) {
+        log.debug("REST request to delete ShoppingCart : {}", id);
+        shoppingCartService.delete(id);
+        return ResponseEntity
+            .noContent()
+            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString()))
+            .build();
+    }
+
+
+    /**
+     * {@code GET  /shopping-carts/current-user} : get the active shoppingCart of current user.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the shoppingCart, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/shopping-carts/current-user-active")
+    public ResponseEntity<ShoppingCart> getActiveShoppingCartByUser() {
+        String user = SecurityUtils.getCurrentUserLogin().orElse("");
+        log.debug("REST request to get ShoppingCart for user: {}", user);
+        return ResponseEntity.ok().body(shoppingCartService.findActiveCartByUser(user));
+    }
+
+    /**
+     * {@code GET  /shopping-carts/current-user} : get all shoppingCarts of current user.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the shoppingCart, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/shopping-carts/current-user")
+    public List<ShoppingCart> getShoppingCartByUser() {
+        String user = SecurityUtils.getCurrentUserLogin().orElse("");
+        log.debug("REST request to get ShoppingCart for user: {}", user);
+        return shoppingCartService.findCartsByUser(user);
+    }
+
+
+    /**
+     * {@code PUT /shopping-carts/add-product/:id} : Add a product to active shoppingCart of current user
+     *
+     * @param id the id of the product to add.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated shoppingCart,
+     * or with status {@code 400 (Bad Request)} if the shoppingCart is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the shoppingCart couldn't be updated.
+     * @throws EntityNotFoundException if the product is not found.
+     */
+    @PutMapping("/shopping-carts/add-product/{id}")
+    public ResponseEntity<ShoppingCart> addProduct(@PathVariable Long id) throws EntityNotFoundException {
+        log.debug("REST request to add product to ShoppingCart");
+        String user = SecurityUtils.getCurrentUserLogin().orElseThrow(() -> new EntityNotFoundException("User"));
+        ShoppingCart result = shoppingCartService.addProductForUser(id, user);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code DELETE /shopping-carts/remove-order/:id} : Remove a product order from active shoppingCart of current user
+     *
+     * @param id the id of the product order to remove.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated shoppingCart,
+     * or with status {@code 400 (Bad Request)} if the shoppingCart is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the shoppingCart couldn't be updated.
+     * @throws EntityNotFoundException if the order is not found.
+     */
+    @DeleteMapping("/shopping-carts/remove-order/{id}")
+    public ResponseEntity<ShoppingCart> removeOrder(@PathVariable Long id) throws EntityNotFoundException {
+        log.debug("REST request to remove product order from ShoppingCart");
+        String user = SecurityUtils.getCurrentUserLogin().orElseThrow(() -> new EntityNotFoundException("User"));
+        ShoppingCart result = shoppingCartService.removeProductOrderForUser(id, user);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PUT  /close} : Close an existing shoppingCart.
+     *
+     * @param paymentType the payment type used.
+     * @param paymentRef  the payment reference from PSP.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated shoppingCart,
+     * or with status {@code 400 (Bad Request)} if the shoppingCart is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the shoppingCart couldn't be updated.
+     * @throws EntityNotFoundException if the order is not found.
+     */
+    @PutMapping("/shopping-carts/close")
+    public ResponseEntity<ShoppingCart> closeShoppingCart(@RequestParam String paymentType, @RequestParam String paymentRef, @RequestParam OrderStatus status) throws EntityNotFoundException {
+        log.debug("REST request to update ShoppingCart");
+        String user = SecurityUtils.getCurrentUserLogin().orElseThrow(() -> new EntityNotFoundException("User"));
+        ShoppingCart result = shoppingCartService.updateCartWithPayment(user, paymentType, paymentRef, status);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+}
